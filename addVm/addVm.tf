4e6655cb41e725aace35fd194e87f8f8dde2d8f8
CarMarketDev <170471777+CarMarketDev@users.noreply.github.com>
2024-07-07 17:28:52 -0400

    added config sections for VM and NSG

--- addVm/addVm.tf
+++ addVm/addVm.tf
@@ -32,3 +32,70 @@ resource "azurerm_resource_group" "rg" {
   tags = local.commonTags
 }
 
+resource "azurerm_virtual_network" "vnet1" {
+  name = "vnet1"
+  address_space = ["10.0.0.0/16"]
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet1"{
+  name = "internal"
+  resource_group_name = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "vnetInt1" {
+  name = "vmTestNic"
+  location = azurerm_resource_group.rg.location
+  resource_grou_name = azurerm_resource_group.rg.name 
+
+  ip_configuration {
+    name = "internal"
+    subnet_id = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "tfTestVM" {
+  name                = "tfTestVM"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "B1s"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_network_security_group" "testNSG" {
+  name                = "nsgTestForVM1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "vMNSG1"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
