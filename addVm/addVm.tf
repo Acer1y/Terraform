e3fde75ebebf22e82a847a64740050b8fe8fa405
CarMarketDev <170471777+CarMarketDev@users.noreply.github.com>
2024-07-07 16:22:18 -0400

    Fixed commontags

--- addVm/addVm.tf
+++ addVm/addVm.tf
@@ -1,6 +1,15 @@
 # This will be used to add a VM to Azure. Not free, but good practice! 
 # This will also contain variables for use with vnet and other items. 
 
+# Set locals, this will be for tagging
+locals {
+  location = "useast"
+  commonTags = {
+    createdby = "me"
+    tier = "production"
+  }
+}
+
 # Set the provider, AzureRM in this case for managing resources
 terraform {
   required_providers {
@@ -20,4 +29,5 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "addVMTest"
   location = "eastus"
+  tags = logals.commonTags
 }
