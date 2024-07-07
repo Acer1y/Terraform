f357fd1e6fd56d434eb1867490f396df20d26adf
CarMarketDev <170471777+CarMarketDev@users.noreply.github.com>
2024-07-06 21:11:48 -0400

    Created new branch and added TF file for branch

---
+++ terraformAddResourceGroup.tf
@@ -0,0 +1,22 @@
+# tf file for adding a resource group 
+
+# Set the provider, AzureRM in this case for managing resources
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+    name = "My TF test Group" #intentionally added spaces to see what validate and format do.
+    location = "eastus2"
+}
