ce546343ef19ab2b87a9f8c99c13b9b10a184d66
CarMarketDev <170471777+CarMarketDev@users.noreply.github.com>
2024-07-06 21:07:18 -0400

    Added template for future use

---
+++ terraformTemplate.tf
@@ -0,0 +1,17 @@
+# This is a template for use with Terraform. 
+
+# Set the provider, AzureRM in this case for managing resources
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
